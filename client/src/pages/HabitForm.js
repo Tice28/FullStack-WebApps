81315c020bbef11539215602b599b372e2613a2b
trisston <trisstonprogramming@gmail.com>
2024-10-11 21:24:48 -0500

    Create Clientside Routing

---
+++ client/src/pages/HabitForm.js
@@ -0,0 +1,9 @@
+const HabitForm = () => {
+  return (
+    <>
+      <h1>HabitForm</h1>
+    </>
+  );
+};
+
+export default HabitForm;
---
+++ client/src/pages/Layout.js
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/habit">Create Habit</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
